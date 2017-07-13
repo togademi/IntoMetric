6f9b65743c4224b3211274d874dfdd8b3854d297
Tomas Garcia de Miguel <togademi@gmail.com>
2017-07-13 21:30:42 +0200

    First clean start

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # IntoMetric
 
 📐 Reddit bot that converts imperial measurements into the metric system.
+
+### Pending tasks
+
+* [ ] Do the conversion of every unit (most of)
+* [ ] Comment using the appropriate Reddit markdown formatting (put small text)
+* [ ] Fix the post limit, so that it waits between each comment
+* [ ] Fix the string/float thing with commas and dots
+* [ ] Reduce number of decimal numbers displayed
--- bot-IntoMetric.py
+++ bot-IntoMetric.py
@@ -1,34 +1,33 @@
 import praw
+import os
 
 reddit = praw.Reddit('IntoMetric')
 
-pfe = reddit.subreddit('pythonforengineers')
+# Have we run this code before? If not, create an empty list
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
 
-for comment in pfe.stream.comments():
-	# If we haven't replied to this post before
-	# if submission.id not in posts_replied_to:
+# If we have run the code before, load the list of posts we have replied to
+else:
+    # Read the file into a list and remove any empty values
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
+
+im = reddit.subreddit('IntoMetric')
+
+for comment in im.stream.comments():
+
     text = comment.body
     author = comment.author
-    list_of_words = text.split()
 
-    if (' inches' in text.lower()):
-        comment.reply("IntoMetric verifies")
+    if (' inches' in text.lower() and author != 'IntoMetric'):
+        list_of_words = text.split()
 
-
-# for comment in bot.subreddit('pythonforengineers').stream.comments():
-#     text = comment.body
-#     author = comment.author
-#     list_of_words = text.split()
-#
-#     if (' inches ' in text.lower()):
-#         comment.reply("IntoMetric verifies")
-
-    # text = comment.body
-    # author = comment.author
-    # list_of_words = text.split()
-    # print list_of_words
-    #
-    # if (' inches ' in text.lower()):
-    #     prev_word = list_of_words[list_of_words.index('inches') - 1]
-    #     message = "that is " + prev_word + ' cm'
-    #     comment.reply(message)
+        # If we haven't replied to this post before
+        if comment.id not in posts_replied_to:
+            prev_word = list_of_words[list_of_words.index('inches')-1]
+            # Remove dots and change decimal commas to dots
+            prev_num = float((prev_word.replace('.','')).replace(',','.'))
+            comment.reply(str(prev_num)+" inches = "+str(prev_num*2.54)+" cm")
