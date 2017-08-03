3865c37722a82b720069e892f8d6c7fbf13b6a16
Tomas Garcia de Miguel <togademi@gmail.com>
2017-08-03 14:18:50 +0200

    mm

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import praw
 import os
 
+with open('/home/tomas/IntoMetric/posts_replied_to.txt') as f:
+posts_replied_to = open('posts_replied_to.txt','r+')
+
 reddit = praw.Reddit('IntoMetric')
 
 # Have we run this code before? If not, create an empty list
@@ -17,24 +20,6 @@ else:
 
 im = reddit.subreddit('IntoMetric')
 
-# for comment in im.stream.comments():
-#
-#     text = comment.body
-#     author = comment.author
-#
-#     if (' inches' in text.lower() and author != 'IntoMetric'):
-#         list_of_words = text.split()
-#
-#         # If we haven't replied to this post before
-#         if comment.id not in posts_replied_to:
-#             prev_word = list_of_words[list_of_words.index('inches')-1]
-#             # Remove dots and change decimal commas to dots
-#             prev_num = float((prev_word.replace('.','')).replace(',','.'))
-#             comment.reply(str(prev_num)+" inches = "+str(prev_num*2.54)+" cm")
-
-
-# ---------------------------------- Draft ---------------------------------------
-
 units_imp = ["inches", "feet", "miles", "gallons", "pounds", "ounces", "mph"]
 units_met = ["cm", "m", "km", "L", "kg", "g", "km/h"]
 multip = [2.54, 0.3048, 1.60934, 3.78541, 0.453592, 28.3495, 1.60934]
@@ -46,21 +31,8 @@ def is_number(s):
     except ValueError:
         return False
 
-# If we haven't replied to this post before
-# if comment.id not in posts_replied_to:
-# 	# Remove dots and change decimal commas to dots
-# 	prev_word = (list_of_words[list_of_words.index('inches')-1]
-# 	prev_word_clean = (prev_word.replace('.','')).replace(',','.')
-# 	#If the string is numeric
-# 	if is_number(prev_word_clean):
-# 		# Remove dots and change decimal commas to dots
-# 		prev_num = float(prev_word)
-# 		# Add a circumflex at the beginning to make text smaller
-# 		comment.reply("^"+prev_word.replace('.',',')+" inches = "+str(prev_num*2.54)+" cm")
+print posts_replied_to
 
-
-
-## ------ Trying with the for loop over the list-------------
 for comment in im.stream.comments():
     author = comment.author
     if (author!='IntoMetric' and comment.id not in posts_replied_to):
@@ -73,3 +45,5 @@ for comment in im.stream.comments():
             	if is_number(num_usable):
                     num_eur = num_usable.replace('.',',')
                     comment.reply(num_eur+" "+units_imp[u]+" = "+str(float(num_usable)*multip[u])+" "+units_met[u])
+                    posts_replied_to.append(comment.id)
+                    print posts_replied_to
